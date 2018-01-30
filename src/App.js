d04fb245c25d31371f339f10f96d88eba8394b75
Krystof Celba <krystof.celba@icloud.com>
2018-01-30 19:12:47 +0100

    feat: add tab based app example

--- src/App.js
+++ src/App.js
@@ -3,9 +3,26 @@ import { registerScreens } from './screens';
 
 registerScreens();
 
-Navigation.startSingleScreenApp({
+/* Navigation.startSingleScreenApp({
   screen: {
     screen: 'example.First',
     title: 'Welcome',
   },
+}); */
+
+Navigation.startTabBasedApp({
+  tabs: [
+    {
+      label: 'One', // tab label as appears under the icon in iOS (optional)
+      screen: 'example.First', // unique ID registered with Navigation.registerScreen
+      icon: require('./images/one.png'), // local image asset for the tab icon unselected state (optional on iOS)
+      title: 'Screen One', // title of the screen as appears in the nav bar (optional)
+    },
+    {
+      label: 'Second', // tab label as appears under the icon in iOS (optional)
+      screen: 'example.Second', // unique ID registered with Navigation.registerScreen
+      icon: require('./images/one.png'), // local image asset for the tab icon unselected state (optional on iOS)
+      title: 'Screen Two', // title of the screen as appears in the nav bar (optional)
+    },
+  ],
 });
